4b6bb78b84df6cd12ea34c373798dfa7de9322f0
Viacheslav <slava.p12@yandex.ru>
2021-08-21 17:11:33 +0300

    Initial Commit

---
+++ ChangeColorView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangeColorView
+//
+//  Created by Слава Платонов on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
